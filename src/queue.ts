1ec3ae1edcf94d2be97e4cf7cef7b087fb1a515a
WMXPY <wm@mengw.io>
2020-04-16 11:57:27 -0500

    implement print queue

--- src/queue.ts
+++ src/queue.ts
@@ -3,3 +3,37 @@
  * @namespace Print
  * @description Queue
  */
+
+import { PrintOptions, printReactElementAsBody, printReactElementAsPage } from "./print";
+
+export class PrintQueue {
+
+    public static create(): PrintQueue {
+
+        return new PrintQueue();
+    }
+
+    private _loading: boolean;
+
+    private constructor() {
+
+        this._loading = false;
+    }
+
+    public get loading(): boolean {
+
+        return this._loading;
+    }
+
+    public async printReactElementAsBody(element: React.ReactElement, options: PrintOptions = {}): Promise<this> {
+
+        await printReactElementAsBody(element, options);
+        return this;
+    }
+
+    public async printReactElementAsPage(element: React.ReactElement, options: PrintOptions = {}): Promise<this> {
+
+        await printReactElementAsPage(element, options);
+        return this;
+    }
+}
